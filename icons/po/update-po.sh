909150b925b9c5fbe099de3f8a4b6e24e4cf384c
Graeme Gott <graeme@gottcode.org>
2018-05-28 14:58:07 -0400

    Split script for updating translations.

--- icons/po/update-po.sh
+++ icons/po/update-po.sh
@@ -4,7 +4,6 @@
 echo -n 'Preparing files...'
 cd ..
 
-rm -f tanglet.desktop.in.h
 rm -f tanglet.desktop.in
 cp tanglet.desktop tanglet.desktop.in
 sed -e '/^Name\[/ d' \
@@ -14,9 +13,7 @@ sed -e '/^Name\[/ d' \
 	-e 's/^GenericName/_GenericName/' \
 	-e 's/^Comment/_Comment/' \
 	-i tanglet.desktop.in
-intltool-extract --quiet --type=gettext/ini tanglet.desktop.in
 
-rm -f tanglet.appdata.xml.in.h
 rm -f tanglet.appdata.xml.in
 cp tanglet.appdata.xml tanglet.appdata.xml.in
 sed -e '/p xml:lang/ d' \
@@ -29,26 +26,15 @@ sed -e '/p xml:lang/ d' \
 	-e 's/<name>/<_name>/' \
 	-e 's/<\/name>/<\/_name>/' \
 	-i tanglet.appdata.xml.in
-intltool-extract --quiet --type=gettext/xml tanglet.appdata.xml.in
 
-rm -f tanglet.xml.in.h
 rm -f tanglet.xml.in
 cp tanglet.xml tanglet.xml.in
 sed -e '/comment xml:lang/ d' \
 	-e 's/<comment>/<_comment>/' \
 	-e 's/<\/comment>/<\/_comment>/' \
 	-i tanglet.xml.in
-intltool-extract --quiet --type=gettext/xml tanglet.xml.in
 
 cd po
-echo ' DONE'
-
-
-echo -n 'Extracting messages...'
-xgettext --from-code=UTF-8 --c++ --keyword=_ --keyword=N_:1 \
-	--package-name='Tanglet' --copyright-holder='Graeme Gott' \
-	--output=description.pot ../*.h
-sed 's/CHARSET/UTF-8/' -i description.pot
 echo ' DONE'
 
 
@@ -65,15 +51,14 @@ echo -n 'Merging translations...'
 cd ..
 
 intltool-merge --quiet --desktop-style po tanglet.desktop.in tanglet.desktop
-rm -f tanglet.desktop.in.h
 rm -f tanglet.desktop.in
 
 intltool-merge --quiet --xml-style po tanglet.appdata.xml.in tanglet.appdata.xml
-rm -f tanglet.appdata.xml.in.h
+echo >> tanglet.appdata.xml
 rm -f tanglet.appdata.xml.in
 
 intltool-merge --quiet --xml-style po tanglet.xml.in tanglet.xml
-rm -f tanglet.xml.in.h
+echo >> tanglet.xml
 rm -f tanglet.xml.in
 
 echo ' DONE'
